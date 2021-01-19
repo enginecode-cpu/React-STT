e0b7740bcc26859868c0359988e80cfcab15e5e9
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-19 18:13:51 +0900

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = "ko-KR";
 
 function App() {
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState(null);
+  const [savedNote, setSavedNote] = useState([]);
+
+  const handleListen = useCallback(() => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue...");
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log("Stoped Mic on Click");
+      };
+    }
+    mic.onstart = () => {
+      console.log("Mics on");
+    };
+
+    mic.onresult = (e) => {
+      console.log(e);
+      const transcript = Array.from(e.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(transcript);
+      setNote(transcript);
+      mic.onerror = (e) => {
+        console.log(e.error);
+      };
+    };
+  }, [isListening]);
+
+  const handleSaveNote = useCallback(() => {
+    setSavedNote([...savedNote, note]);
+    setNote("");
+  }, [note, savedNote]);
+
+  useEffect(() => {
+    handleListen();
+    // eslint-disable-next-line
+  }, [isListening]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Voice Note</h1>
+      <div className="container">
+        <div className="box curent-note">
+          <h2>Current Note</h2>
+          {isListening ? <span>🎙</span> : <span>🔴</span>}
+          <button className="save" onClick={handleSaveNote} disabled={!note}>
+            Save Note
+          </button>
+          <button
+            className="start-stop"
+            onClick={() => setIsListening((prevState) => !prevState)}
+          >
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="box save-note">
+          <h2>Note</h2>
+          {savedNote.map((n) => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
